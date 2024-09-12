ea3d3b2f47ab3c75e5ea400327392e0917dcbc93
rodolfo.arias <rodolfo.arias@utp.ac.pa>
2024-09-11 19:32:16 -0500

    Agregar archivos de proyecto.

---
+++ laboratorio5.1/Program.cs
@@ -0,0 +1,28 @@
+﻿private int[] sueldos;
+
+public void Cargar()
+{
+    sueldos = new int[6];
+    for(int f = 1; f<=5; f++)
+    {
+        Console.Write("ingrese sueldo del operario " + f + ":");
+        String linea;
+        linea = Console.ReadLine();
+        sueldos[f] = int.Parse(linea);
+    }
+}
+public void imprimir()
+{
+    Console.WriteLine("los 5 sueldos de los operarios \n");
+    for(int f = 1; f <= 5; f++)
+    {
+        Console.WriteLine("[" + sueldos[f] + "]");
+    }
+    Console.ReadKey();
+}
+static void Main(string[] args)
+{
+    PruebaVector1 pv = new PruebaVector1();
+    pv.Cargar();
+    pv.imprimir();
+}
